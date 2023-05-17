8244f7a858d6ab4c3aef679694c5518f7ffa7bea
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2023-05-17 09:51:46 +0200

    database perfect edition

--- DB TIW/db_TIW2.0.sql
+++ DB TIW/db_TIW2.0.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for macos13 (x86_64)
 --
--- Host: 127.0.0.1    Database: db_tiw
+-- Host: 127.0.0.1    Database: db_TIW2.0
 -- ------------------------------------------------------
 -- Server version	8.0.33
 
@@ -16,82 +16,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `category`
---
-
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `category` (
-  `idCategory` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCategory`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `category`
---
-
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (9,'Materiali solidi'),(91,'Materiali inerti'),(92,'Materiali Ferrosi'),(93,'Kryptonite'),(94,'Ossidiana'),(95,'Diamanti'),(911,'Inerti da edilizia'),(912,'Inerti Ceramici'),(9111,'Amianto'),(9112,'Materiali cementizi'),(9121,'Piastrelle'),(9122,'Sanitari'),(91111,'Amianto in lastre'),(91112,'Amianto in frammenti');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `relationship`
---
-
-DROP TABLE IF EXISTS `relationship`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `relationship` (
-  `fatherID` int NOT NULL,
-  `childID` int NOT NULL,
-  PRIMARY KEY (`fatherID`,`childID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `relationship`
---
-
-LOCK TABLES `relationship` WRITE;
-/*!40000 ALTER TABLE `relationship` DISABLE KEYS */;
-INSERT INTO `relationship` VALUES (9,91),(9,92),(91,911),(91,912),(911,9111),(911,9112),(912,9121),(912,9122),(9111,91111),(9111,91112);
-/*!40000 ALTER TABLE `relationship` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` int NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `surname` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'baker','pane','erizio','fonnesu'),(2,'genova','belin','lorenzo','fonnesu'),(3,'jew','GDL','eliahu','cohen'),(4,'disabile','ballsuck','giovanni','di lorenzo'),(5,'goat','perfezione','luca','guffanti'),(6,'andrew','fats','andrea','grassi'),(7,'chad','cologno','marco','gervatini');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'db_tiw'
+-- Dumping routines for database 'db_TIW2.0'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -103,4 +28,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-15 22:03:44
+-- Dump completed on 2023-05-17  9:49:48
