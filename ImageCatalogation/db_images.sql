326ceea480596a78059313a59c5b5f85c33092e3
Eliahu Cohen <126007216+EliahuC@users.noreply.github.com>
2023-05-18 14:24:48 +0100

    db

--- ImageCatalogation/db_images.sql
+++ ImageCatalogation/db_images.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `db_images` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `db_images`;
--- MySQL dump 10.13  Distrib 8.0.33, for macos13 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: db_images
 -- ------------------------------------------------------
@@ -18,14 +18,14 @@ USE `db_images`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Category`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `Category`;
+DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Category` (
-  `id` int NOT NULL,
+CREATE TABLE `category` (
+  `id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `existence` (`id`)
@@ -33,13 +33,13 @@ CREATE TABLE `Category` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Category`
+-- Dumping data for table `category`
 --
 
-LOCK TABLES `Category` WRITE;
-/*!40000 ALTER TABLE `Category` DISABLE KEYS */;
-INSERT INTO `Category` VALUES (9,'materiali solidi'),(91,'materiali inerti'),(92,'materiali ferrosi'),(911,'inerti da edilizia'),(912,'ineri ceramici'),(9111,'amianto'),(9112,'materiali cementizi'),(9121,'piastrelle'),(9122,'sanitari'),(91111,'amianto in lastre'),(91112,'amianto in frammenti');
-/*!40000 ALTER TABLE `Category` ENABLE KEYS */;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES ('9','materiali solidi'),('91','materiali inerti'),('911','inerti da edilizia'),('9111','amianto'),('91111','amianto in lastre'),('91112','amianto in frammenti'),('9112','materiali cementizi'),('9113','cumma'),('912','ineri ceramici'),('9121','piastrelle'),('9122','sanitari'),('91221','bidet'),('92','materiali ferrosi'),('921','il mio cazzo');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -50,12 +50,12 @@ DROP TABLE IF EXISTS `relationships`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `relationships` (
-  `father` int NOT NULL,
-  `child` int NOT NULL,
+  `father` varchar(45) NOT NULL,
+  `child` varchar(45) NOT NULL,
   PRIMARY KEY (`father`,`child`),
   KEY `child_idx` (`child`),
-  CONSTRAINT `child` FOREIGN KEY (`child`) REFERENCES `Category` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `father` FOREIGN KEY (`father`) REFERENCES `Category` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `childID` FOREIGN KEY (`child`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fatherID` FOREIGN KEY (`father`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -65,7 +65,7 @@ CREATE TABLE `relationships` (
 
 LOCK TABLES `relationships` WRITE;
 /*!40000 ALTER TABLE `relationships` DISABLE KEYS */;
-INSERT INTO `relationships` VALUES (9,91),(9,92),(91,911),(91,912),(911,9111),(911,9112),(912,9121),(912,9122),(9111,91111),(9111,91112);
+INSERT INTO `relationships` VALUES ('9','91'),('91','911'),('911','9111'),('9111','91111'),('9111','91112'),('911','9112'),('911','9113'),('91','912'),('912','9121'),('912','9122'),('9122','91221'),('9','92'),('92','921');
 /*!40000 ALTER TABLE `relationships` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-17 13:53:16
+-- Dump completed on 2023-05-18 15:24:23
