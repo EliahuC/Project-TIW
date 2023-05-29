96cdee5a4244970702eb525ae5cea03ee9ccba3c
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2023-05-29 10:45:24 +0200

    added imageCatJS

---
+++ ImageCatalogationJS/src/main/java/it/polimi/tiw/filters/LoginChecker.java
@@ -0,0 +1,66 @@
+package it.polimi.tiw.filters;
+
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginChecker
+ */
+public class LoginChecker implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public LoginChecker() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException
+
+    {
+        System.out.print("Login checker filter executing ...\n");
+
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String loginpath = req.getServletContext().getContextPath() + "/index.html";
+
+        HttpSession s = req.getSession();
+        if (s.isNew() || s.getAttribute("user") == null) {
+            res.setStatus(403);
+            res.setHeader("Location", loginpath);
+            System.out.print("Login checker FAILED...\n");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
---
+++ ImageCatalogationJS/target/ImageCatalogationJS-1.0-SNAPSHOT/home.js
@@ -0,0 +1,54 @@
+(function () {
+
+    let categoriesList, creationWizard, pageOrchestrator = new PageOrchestrator();
+    let allCategories = [];
+
+    window.addEventListener("load", () => {
+        if (sessionStorage.getItem("username") == null) {
+            window.location.href = "index.html";
+        } else {
+            pageOrchestrator.start(); // initialize the components
+            pageOrchestrator.refresh(); // display initial content
+        }
+    }, false);
+
+    function PersonalMessage(_name, _surname, messagecontainer) {
+        this.name = _name;
+        this.surname = _surname;
+        this.show = function () {
+            messagecontainer.textContent = "Nice to see you again " + this.name + " " + this.surname;
+        }
+    }
+
+
+
+    function PageOrchestrator() {
+
+        this.start = function () {
+
+            personalMessage = new PersonalMessage(
+                sessionStorage.getItem("name"),
+                sessionStorage.getItem("surname"),
+                document.getElementById("username"));
+            personalMessage.show();
+
+            //categoriesList = new CategoriesList(
+                //document.getElementById("allCategories"));
+            //categoriesList.registerEvents(this);
+
+            //creationWizard = new CreationWizard(
+                //document.getElementById("creationWizard"),
+                //document.getElementById("father"));
+            //creationWizard.registerEvents(this);
+
+        }
+
+        this.refresh = function () {
+            //categoriesList.reset();
+            //creationWizard.reset();
+            //categoriesList.show();
+            //creationWizard.show();
+        }
+    }
+
+})();
